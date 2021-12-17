a889f7769c34388dc378b6cea17c74ed8ae3418a
Kwame <gtwene001@st.ug.edu.gh>
2021-12-17 00:50:43 +0000

    metaMask Moralis Auth

--- components/Login.js
+++ components/Login.js
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import { useMoralis } from "react-moralis";
 
 function Login() {
+  const { authenticate } = useMoralis();
+
   return (
     <div className="bg-black relative ">
       <h1>I am the login screen</h1>
@@ -14,7 +17,10 @@ function Login() {
         />
 
         {/* Login button */}
-        <button className="bg-yellow-500 rounded-lg p-5 font-bold animate-bounce">
+        <button
+          onClick={authenticate}
+          className="bg-yellow-500 rounded-lg p-5 font-bold animate-bounce"
+        >
           Login to the METAVERSE
         </button>
       </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,17 @@
 import "../styles/globals.css";
-//import MoralisProvider from "react-moralis";
+import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
-
-{
-  /* <MoralisProvider
+  return (
+    <MoralisProvider
       appId={process.env.NEXT_PUBLIC_APP_ID}
       serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}
     >
       <Component {...pageProps} />
-    </MoralisProvider>; */
+    </MoralisProvider>
+  );
 }
+
+export default MyApp;
+
+/*  */
